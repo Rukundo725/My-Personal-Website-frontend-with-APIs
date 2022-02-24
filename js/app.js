4348b01942233e7f78bd86518e5c3177d4360968
rukundo725 <gerukundo14@gmail.com>
2022-02-24 18:45:25 +0200

    updated  app.js with a function to add a new project to the firebase throug add blog.html

--- js/app.js
+++ js/app.js
@@ -23,64 +23,64 @@ const projectForm = document.querySelector('#add-new-project-form'); // getting 
 // console.log(messageForm, messageList);
 
 
-// // create element & render message(function to render messages from fire base firestore)
-// function renderMessage(doc){
-//     let div = document.createElement('div');
-//     div.className = 'container';
-//     let name = document.createElement('p');
-//     name.setAttribute("id", "name");
-//     let email = document.createElement('p');
-//     email.setAttribute("id", "email");
-//     let message = document.createElement('p');
-//     message.setAttribute("id", "message");
+// create element & render message(function to render messages from fire base firestore)
+function renderMessage(doc){
+    let div = document.createElement('div');
+    div.className = 'container';
+    let name = document.createElement('p');
+    name.setAttribute("id", "name");
+    let email = document.createElement('p');
+    email.setAttribute("id", "email");
+    let message = document.createElement('p');
+    message.setAttribute("id", "message");
 
-//     div.setAttribute('message-id', doc.id);
-//     name.textContent = doc.data().name;
-//     email.textContent = doc.data().email;
-//     message.textContent = doc.data().message;
+    div.setAttribute('message-id', doc.id);
+    name.textContent = doc.data().name;
+    email.textContent = doc.data().email;
+    message.textContent = doc.data().message;
 
-//     div.appendChild(name);
-//     div.appendChild(email);
-//     div.appendChild(message);
-//     messageList.appendChild(div);
-// }
+    div.appendChild(name);
+    div.appendChild(email);
+    div.appendChild(message);
+    messageList.appendChild(div);
+}
 
-// // getting data(messages) from the firebase
-// db.collection('messages').get().then(snapshot => {
-//     snapshot.docs.forEach(doc => {
-//         renderMessage(doc);
-//     });
-// });
+// getting data(messages) from the firebase
+db.collection('messages').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        renderMessage(doc);
+    });
+});
 
-// // saving data(messages) from the user through the index.html form
-// messageForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     db.collection('messages').add({
-//         name: messageForm.name.value,
-//         email: messageForm.email.value,
-//         message: messageForm.message.value
-//     });
-//     messageForm.name.value = '';
-//     messageForm.email.value = '';
-//     messageForm.message.value = '';
-//     alert("The form was submitted");
-// });
+// saving data(messages) from the user through the index.html form
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('messages').add({
+        name: messageForm.name.value,
+        email: messageForm.email.value,
+        message: messageForm.message.value
+    });
+    messageForm.name.value = '';
+    messageForm.email.value = '';
+    messageForm.message.value = '';
+    alert("The form was submitted");
+});
 
-// // saving data(articles) from the dashboard  to the firebase database
-// articleForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     db.collection('articles').add({
-//         title: articleForm.title.value,
-//         article: articleForm.article.value,
-//         date: articleForm.date.value
-//     }).then(les=>{
-//         console.log(les)
-//     }).catch(error=>{console.log(error.message)})
-//     articleForm.title.value = '';
-//     articleForm.article.value = '';
-//     articleForm.date.value = '';
-//     alert("The article was created");
-// });
+// saving data(articles) from the dashboard  to the firebase database
+articleForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('articles').add({
+        title: articleForm.title.value,
+        article: articleForm.article.value,
+        date: articleForm.date.value
+    }).then(les=>{
+        console.log(les)
+    }).catch(error=>{console.log(error.message)})
+    articleForm.title.value = '';
+    articleForm.article.value = '';
+    articleForm.date.value = '';
+    alert("The article was created");
+});
 
 // saving data(projects) from the dashboard  to the firebase database
 projectForm.addEventListener('submit', (e) => {
@@ -98,13 +98,13 @@ projectForm.addEventListener('submit', (e) => {
     alert("The project was created");
 });
 
-// // login authentication
-// logInForm.addEventListener('submit', (e)=>{
-//     e.preventDefault()
-//     firebase.auth().signInWithEmailAndPassword(logInForm.email.value, logInForm.password.value)
-//     .then((userCredential) => {
-//     console.log("Logged In")
-//     window.open("dashboard.html")
-//   });
+// login authentication
+logInForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    firebase.auth().signInWithEmailAndPassword(logInForm.email.value, logInForm.password.value)
+    .then((userCredential) => {
+    console.log("Logged In")
+    window.open("dashboard.html")
+  });
 
-// });
+});
