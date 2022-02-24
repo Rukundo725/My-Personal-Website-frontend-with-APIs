2a4d3527436ba53c00b1ce6cf066c3c9c2d0a63d
rukundo725 <gerukundo14@gmail.com>
2022-02-24 10:49:54 +0200

    created a function to retrieve messages from firebase store to the dashboard

--- js/app.js
+++ js/app.js
@@ -13,3 +13,35 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+//  declaration of variables 
+const messageList = document.querySelector('#message-list');// getting the messageList id from dash-messageList.html 
+
+// create element & render message(function to render messages from fire base firestore)
+function renderMessage(doc){
+    let div = document.createElement('div');
+    div.className = 'container';
+    let name = document.createElement('p');
+    name.setAttribute("id", "name");
+    let email = document.createElement('p');
+    email.setAttribute("id", "email");
+    let message = document.createElement('p');
+    message.setAttribute("id", "message");
+
+    div.setAttribute('message-id', doc.id);
+    name.textContent = doc.data().name;
+    email.textContent = doc.data().email;
+    message.textContent = doc.data().message;
+
+    div.appendChild(name);
+    div.appendChild(email);
+    div.appendChild(message);
+    messageList.appendChild(div);
+}
+
+// getting data(messages) from the firebase
+db.collection('messages').get().then(snapshot => {
+    snapshot.docs.forEach(doc => {
+        renderMessage(doc);
+    });
+});
