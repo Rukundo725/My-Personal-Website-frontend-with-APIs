7d990ec51b109d470cf02ecf85ee3aaa71a80bd9
rukundo725 <gerukundo14@gmail.com>
2022-02-25 06:46:41 +0200

    moved functions of log in authentication to the login js file

--- js/app.js
+++ js/app.js
@@ -18,13 +18,98 @@ const db = firebase.firestore();
 const messageList = document.querySelector('#message-list');// getting the messageList id from dash-messageList.html 
 const messageForm = document.getElementById('send-message-form'); // getting elements of the form through send-message-form id of the index.html
 const articleForm = document.querySelector('#add-new-article-form'); // getting the content of the blog form through add-new-article-form id of the add-blog.html
-const logInForm = document.querySelector('#log-in-form') // creating a variable to access the log in form from login.html
+const logInForm = document.querySelector('#log-in-form'); // creating a variable to access the log in form from login.html
 const projectForm = document.querySelector('#add-new-project-form'); // getting the content of the project form through add-new-project-form id of the add-project.html
+const commentForm = document.querySelector('#comment-section'); // getting the comment of the user  through comment-section id 
 // console.log(messageForm, messageList);
 
 
-// create element & render message(function to render messages from fire base firestore)
-function renderMessage(doc){
+//renderMessage/ create element & render message(function to render messages from fire base firestore)
+// renderMessage = function(doc){
+//     let div = document.createElement('div');
+//     div.className = 'container';
+//     let name = document.createElement('p');
+//     name.setAttribute("id", "name");
+//     let email = document.createElement('p');
+//     email.setAttribute("id", "email");
+//     let message = document.createElement('p');
+//     message.setAttribute("id", "message");
+
+//     div.setAttribute('message-id', doc.id);
+//     name.textContent = doc.data().name;
+//     email.textContent = doc.data().email;
+//     message.textContent = doc.data().message;
+
+//     div.appendChild(name);
+//     div.appendChild(email);
+//     div.appendChild(message);
+//     messageList.appendChild(div);
+// }
+
+// // getting data(messages) from the firebase
+// db.collection('messages').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderMessage(doc);
+//     });
+// });
+
+// // saving data(messages) from the user through the index.html form
+// messageForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     db.collection('messages').add({
+//         name: messageForm.name.value,
+//         email: messageForm.email.value,
+//         message: messageForm.message.value
+//     });
+//     messageForm.name.value = '';
+//     messageForm.email.value = '';
+//     messageForm.message.value = '';
+//     alert("The form was submitted");
+// });
+
+// // saving data(articles) from the dashboard  to the firebase database
+// articleForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     db.collection('articles').add({
+//         title: articleForm.title.value,
+//         article: articleForm.article.value,
+//         date: articleForm.date.value
+//     });
+//     articleForm.title.value = '';
+//     articleForm.article.value = '';
+//     articleForm.date.value = '';
+//     alert("The article was created");
+// });
+
+// // saving data(projects) from the dashboard  to the firebase database
+// projectForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     db.collection('projects').add({
+//         title: projectForm.title.value,
+//         summary: projectForm.summary.value,
+//         link: projectForm.link.value
+//     });
+//     projectForm.title.value = '';
+//     projectForm.summary.value = '';
+//     projectForm.link.value = '';
+//     alert("The project was created");
+// });
+
+// saving data(comments) from the article  to the firebase database
+commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('comments').add({
+        email: commentForm.email.value,
+        comment: commentForm.comment.value,
+        
+    });
+    commentForm.email.value = '';
+    commentForm.comment.value = '';
+    
+    alert("your comment is highly appreciated");
+});
+
+renderComment = function(doc){
     let div = document.createElement('div');
     div.className = 'container';
     let name = document.createElement('p');
@@ -48,63 +133,7 @@ function renderMessage(doc){
 // getting data(messages) from the firebase
 db.collection('messages').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
-        renderMessage(doc);
+        renderComment(doc);
     });
 });
 
-// saving data(messages) from the user through the index.html form
-messageForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('messages').add({
-        name: messageForm.name.value,
-        email: messageForm.email.value,
-        message: messageForm.message.value
-    });
-    messageForm.name.value = '';
-    messageForm.email.value = '';
-    messageForm.message.value = '';
-    alert("The form was submitted");
-});
-
-// saving data(articles) from the dashboard  to the firebase database
-articleForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('articles').add({
-        title: articleForm.title.value,
-        article: articleForm.article.value,
-        date: articleForm.date.value
-    }).then(les=>{
-        console.log(les)
-    }).catch(error=>{console.log(error.message)})
-    articleForm.title.value = '';
-    articleForm.article.value = '';
-    articleForm.date.value = '';
-    alert("The article was created");
-});
-
-// saving data(projects) from the dashboard  to the firebase database
-projectForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('projects').add({
-        title: projectForm.title.value,
-        summary: projectForm.summary.value,
-        link: projectForm.link.value
-    }).then(les=>{
-        console.log(les)
-    }).catch(error=>{console.log(error.message)})
-    projectForm.title.value = '';
-    projectForm.summary.value = '';
-    projectForm.link.value = '';
-    alert("The project was created");
-});
-
-// login authentication
-logInForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    firebase.auth().signInWithEmailAndPassword(logInForm.email.value, logInForm.password.value)
-    .then((userCredential) => {
-    console.log("Logged In")
-    window.open("dashboard.html")
-  });
-
-});
