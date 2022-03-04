8c32d1bac648577880be5b9b982330df4521e6e6
rukundo725 <gerukundo14@gmail.com>
2022-03-04 13:50:36 +0200

    creted the update blog js

---
+++ js/update-blog.js
@@ -0,0 +1,26 @@
+const updatePost = document.getElementById('edit');
+const title = document.getElementById('title');
+const article = document.getElementById('article');
+
+const id = location.hash.slice(1);
+
+db.collection('articles').doc(id).get().then(res => {
+    title.value = res.data().title;
+    article.value = res.data().article;
+})
+
+updatePost.addEventListener('click',(e)=>{
+    e.preventDefault();
+    db.collection('articles').doc(id).update({
+        article: article.value,
+        date: date.value,
+        title: title.value
+    }).then(res=>{
+        title.value = "";
+        article.value = "";
+        alert("Post updated");
+        location.href = "blog.html";
+    }).catch(err=>{
+        alert("Error: " + err.message)
+    })
+})
